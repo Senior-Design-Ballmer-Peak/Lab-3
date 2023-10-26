02507aea75176d54a01b769ff55adfb6b80ac2b3
Charles Wilmot II <chazwilmot@gmail.com>
2023-10-25 23:23:40 -0500

    Create app.jsx

---
+++ StarterCode/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Team Portfolio</h1>
+            {/* Add more components and functionalities as needed */}
+        </div>
+    );
+}
+
+export default App;
