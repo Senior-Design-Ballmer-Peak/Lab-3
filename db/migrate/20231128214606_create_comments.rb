544f4d20ae9e3c72ec65d2b3ff926daf9a27a20d
teckenrod <treyeck2@gmail.com>
2023-11-28 16:15:30 -0600

    creating comments

--- db/migrate/20231128214606_create_comments.rb
+++ db/migrate/20231128214606_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[7.1]
   def change
     create_table :comments do |t|
-      t.text :comment
-      t.references :user, foreign_key: true
+      t.string :name
+      t.text :content
 
       t.timestamps
     end
