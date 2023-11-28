56da8d0a190943f1303de4092a15e2925ef205df
teckenrod <treyeck2@gmail.com>
2023-11-28 14:05:53 -0600

    added photos

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
   get 'tristan/index'
   get 'charlie/index'
   get 'home/index'
+
+  get '/miles/index', to: 'miles#index', as: 'miles_index'
+  get '/trey/index', to: 'trey#index', as: 'trey_index'
+  get '/tristan/index', to: 'tristan#index', as: 'tristan_index'
+  get '/charlie/index', to: 'charlie#index', as: 'charlie_index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
