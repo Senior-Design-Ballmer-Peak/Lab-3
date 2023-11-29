a83055b5266e6ff2ae19174d2e156ff3a410ccb4
teckenrod <treyeck2@gmail.com>
2023-11-29 10:53:45 -0600

    Testing heroku

---
+++ public/assets/comments-742d9c57cebccd8498a52f2492262ebb65c5dcb97b84675f8b0014bccd9826ee.js
@@ -0,0 +1,27 @@
+// comments.js
+
+function submitCommentToServer() {
+    const name = document.getElementById('name').value;
+    const comment = document.getElementById('comment').value;
+
+    // Send a request to your Rails server to create a new comment
+    fetch('/comments', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            name: name,
+            content: comment,
+        }),
+    })
+        .then(response => response.json())
+        .then(data => {
+            // Handle the response (e.g., update the UI with the new comment)
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+};
