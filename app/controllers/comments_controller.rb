5ea88fda91ecc5f18798cab9f5eaaeb7c8008104
Tristan Pawlenty <tristan-pawlenty@uiowa.edu>
2023-11-29 10:51:40 -0600

    i hate charlie

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,16 +3,18 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
-    if @comment.save
-      render json: @comment, status: :created
-    else
-      render json: @comment.errors, status: :unprocessable_entity
-    end
+    @comment.save
+    redirect_to root_path
+  end
+
+  def index
+    @comments = Comment.all
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:name, :content)
+    params.require(:comment).permit(:name, :comment)
   end
 end
+
