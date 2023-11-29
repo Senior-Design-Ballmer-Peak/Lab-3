db1ade5a52408b69ffffa1915f7032b6d5adcb99
teckenrod <treyeck2@gmail.com>
2023-11-29 11:43:24 -0600

    comments working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  before_action :authenticate_user!, only: [:create]  # Ensure the user is signed in for comment creation
+  before_action :authenticate_user!, only: [:create]
 
   def create
     @comment = current_user.comments.build(comment_params)
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_29_060812) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_29_174102) do
   create_table "comments", force: :cascade do |t|
-    t.string "name"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
