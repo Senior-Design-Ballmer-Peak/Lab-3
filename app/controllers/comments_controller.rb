9ffee2a116f3b8876bd6bb2c56ed72badb7c1e8b
Tristan Pawlenty <tristan-pawlenty@uiowa.edu>
2023-11-29 10:04:08 -0600

    zip bomb

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+# app/controllers/comments_controller.rb
+
+class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      render json: @comment, status: :created
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:name, :content)
+  end
+end
