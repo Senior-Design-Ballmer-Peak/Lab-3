5e9064311b00b0ac9ab1fbb8267c1be17ff9b52f
teckenrod <treyeck2@gmail.com>
2023-11-29 00:11:12 -0600

    rebase

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_29_174102) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_29_060812) do
   create_table "comments", force: :cascade do |t|
+    t.string "name"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
